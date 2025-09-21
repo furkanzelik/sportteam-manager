e473d107b9a16216050d2da8360971adf69b20ae
Furkan <113936184+furkanzelik@users.noreply.github.com>
2025-09-21 13:19:23 +0200

    feat(auth): add Role enum and role column to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\Role;
 
 class User extends Authenticatable
 {
@@ -31,6 +32,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'role' => Role::class, // automatisch casten naar enum
     ];
 
     /**
